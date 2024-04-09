3b9fb4785f806a0ad3c16cfea9088854ad08928d
Your Name <your@email.com>
2024-04-09 10:03:37 +0530

    workdone

---
+++ portfolio/portfolioapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import ContactMessage
+from django.contrib import messages
+def index(request):
+    if request.method=='POST':
+        Name=request.POST['Name']
+        Email=request.POST['Email']
+        Description=request.POST['Description']
+        ContactMessage.objects.create(name=Name, email=Email, message=Description)
+        messages.success(request,'Data has been submitted')
+        
+
+    return render(request,'index.html')
